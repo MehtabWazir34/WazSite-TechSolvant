88018924a0303d41c3ebb085b5b1fff6cd208f60
MehtabWazir34 <mehtabwazir34@gmail.com>
2025-08-24 23:52:37 +0500

    updates to image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,8 @@ import Us from "../components/Us";
 import RequestForm from "../components/RequestForm";
 import Reviews from "../components/Reviews";
 
+import MyImge from '../assets/Me.png'
+
 const previewServices = [
   { id: 1, type: "Graphics Designs", info: "Quality designs.", card: "/Images/graphics.jpg" },
   { id: 2, type: "Web Development", info: "Modern responsive websites.", card: "https://img.freepik.com/premium-psd/web-development-3d-illustration_609002-519.jpg" },
@@ -14,7 +16,7 @@ const previewServices = [
 
 const previewTeam = [
   { id: 1, title: "SaifUllah Khan", cate: "Software Engineer", pic: "https://media.licdn.com/dms/image/v2/D4D03AQHote-snDKK0g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730812444229?e=1758758400&v=beta&t=JmeAPPLnK8fjAHw1fE_lzdOIFz1-4tQRtnY8DupTHvk" },
-  { id: 2, title: "Mehtab Wazir", cate: "Front-end Developer", pic: "import.meta.env.Me.jpg" },
+  { id: 2, title: "Mehtab Wazir", cate: "Front-end Developer", pic: {MyImge} },
   { id: 3, title: "Nehal Anees", cate: "On-Page SEO Specialist", pic: "https://media.licdn.com/dms/image/v2/D4D03AQFWhNIhJP21zQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1731560468456?e=1758758400&v=beta&t=qnExAn1tEvmDa8upnnEcRr5hZTPR1XvkFv-9qHq5rKU" }
 ];
 
